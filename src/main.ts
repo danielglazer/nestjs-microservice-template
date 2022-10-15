1c8c487c69106cca5899a09213e9f14ef983fb34
Daniel Glazer <danielglazer123@gmail.com>
2022-10-15 19:07:20 +0300

    use compression middeleware in server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import compression from '@fastify/compress';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -8,8 +9,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
+  //For high-traffic websites in production compression should be on reverse proxy and not app server, see https://docs.nestjs.com/techniques/compression
+  await app.register(compression, { encodings: ['gzip', 'deflate'] });
   await app.listen(3000);
 }
 bootstrap();
